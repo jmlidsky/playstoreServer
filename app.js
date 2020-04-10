a55fedd09e48e9225575aac7f06ca662adbe6486
Jill Lidsky <jilllidsky15@gmail.com>
2020-04-10 14:56:02 -0400

    added files

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('common'));
+
+const apps = require('./playstore.js');
+
+app.get('/apps', (req, res) => {
+    const { sort, genres = '' } = req.query;
+
+    if (sort) {
+        if (!['Rating', 'App'].includes(sort)) {
+            return res
+                .status(400)
+                .send('Sort must be one of rating or app');
+        }
+    }
+
+    if (genres) {
+        if (!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genres)) {
+            return res
+                .status(400)
+                .send('Genre must be one of the following: Action, Puzzle, Strategy, Casual, Arcade, Card')
+        }
+    }
+
+    let results = apps
+        .filter(app =>
+            app
+                .Genres
+                .toLowerCase()
+                .includes(genres.toLowerCase())
+        )
+
+
+    if (sort === 'Rating') {
+        results
+            .sort((a, b) => {
+                return a[sort] < b[sort] ? 1 : a[sort] > b[sort] ? -1 : 0;
+            })
+    }
+
+    if (sort === 'App') {
+        results
+            .sort((a, b) => {
+                return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+            })
+    }
+
+    res
+        .json(results);
+});
+
+module.exports = app;
